11be31fb37ebc57300a2f6446dbfbbc4c0c32bcf
anaberst <anavberst@gmail.com>
2025-07-08 15:39:30 -0700

    Initialize Movie class

--- StreamingGuide.py
+++ StreamingGuide.py
@@ -8,3 +8,18 @@
         ## StreamingGuide, representing a guide for finding a specific movie.
     # StreamingGuide contains a list of StreamingService objects.
     # StreamingService contains a dictionary of Movie objects.
+
+class Movie:
+    """
+    Represents an individual movie.
+    """
+    def __init__(self, title, genre, director, year):
+        """
+        Initializes a movie object with four private data members:
+        title, genre, director, and release year.
+        """
+        self._title = title
+        self._genre = genre
+        self._director = director
+        self._year = year
+
