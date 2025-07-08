f0bb7a2ef7346124cba79dcc09b04e00f4960a9e
anaberst <anavberst@gmail.com>
2025-07-08 16:20:17 -0700

    Add GitHub upload date in header

--- StreamingGuide.py
+++ StreamingGuide.py
@@ -1,6 +1,6 @@
 # Author: Anastasiya Berst
 # GitHub username: anaberst
-# Date: 12/05/2024
+# Date: 12/05/2024 (uploaded to GitHub: 07/08/2025)
 # Description:
     # This program contains three classes:
         ## Movie, representing a movie.
