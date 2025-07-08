089ba94eefc25af23a89bbd535cc4d05766fec16
anaberst <anavberst@gmail.com>
2025-07-08 15:29:19 -0700

    Add mandatory CS161 header

---
+++ StreamingGuide.py
@@ -0,0 +1,10 @@
+# Author: Anastasiya Berst
+# GitHub username: anaberst
+# Date: 12/05/2024
+# Description:
+    # This program contains three classes:
+        ## Movie, representing a movie.
+        ## StreamingService, representing a movie streaming platform.
+        ## StreamingGuide, representing a guide for finding a specific movie.
+    # StreamingGuide contains a list of StreamingService objects.
+    # StreamingService contains a dictionary of Movie objects.
