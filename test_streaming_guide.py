1977782980d81822e0af2d5201e10a15215b222f
anaberst <anavberst@gmail.com>
2025-07-08 17:57:57 -0700

    Add mandatory CS161 header to test file

---
+++ test_streaming_guide.py
@@ -0,0 +1,5 @@
+# Author: Anastasiya Berst
+# GitHub username: anaberst
+# Date: 12/05/2024 (uploaded to GitHub: 07/08/2025)
+# Description:
+    # This program contains unit tests for streaming_guide.py
